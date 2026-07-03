67a5f9e939bb2dd6ffb61117526329cb4e65a2ea
ashish-langhe <langheashish389@gmail.com>
2026-07-03 10:18:43 +0530

    chore: configure timestamp camera iOS project

---
+++ TimeStampCamera/TimeStampCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeStampCameraApp.swift
+//  TimeStampCamera
+//
+//  Created by Ashish Langhe on 30/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TimeStampCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppRootView()
+        }
+    }
+}
